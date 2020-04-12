210abf64816bf14897948d468e0c12f1f59a4d36
Andy Walls <andy@silverblocksystems.net>
2020-04-12 12:46:17 -0400

    Disable inclusion of devel.mk to turn off -Werror compile flag

--- mk/syslinux.mk
+++ mk/syslinux.mk
@@ -79,7 +79,7 @@ UMAKEDEPS = -Wp,-MT,$@,-MMD,$(dir $@).$(notdir $@).d
 
 # Items that are only appropriate during development; this file is
 # removed when tarballs are generated.
--include $(MAKEDIR)/devel.mk
+#-include $(MAKEDIR)/devel.mk
 
 # Local additions, like -DDEBUG can go here
 -include $(MAKEDIR)/local.mk
